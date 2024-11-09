26ea431aa0b450137b5a783cb8211ac6a2bb1cbb
NC0312 <niketchawla983@gmail.com>
2024-11-09 10:10:54 +0530

    Searchbar text made more sexier

--- src/my-trips/index.jsx
+++ src/my-trips/index.jsx
@@ -48,7 +48,7 @@ function MyTrips() {
         <div className='relative'>
           <input
             type="text"
-            placeholder="Search destinations..."
+            placeholder="Browse your saved escapes..."
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
             className='pl-10 pr-4 py-2 border border-gray-200 rounded-lg w-64 focus:outline-none focus:ring-2 focus:ring-[#7139f4] bg-white shadow-sm'

55874d2b56188d54af46e123a2a6f79f82f03632
NC0312 <niketchawla983@gmail.com>
2024-11-11 22:19:52 +0530

    Footer 2nd update

--- src/view-trip/components/Footer.jsx
+++ src/view-trip/components/Footer.jsx
@@ -3,12 +3,14 @@ import { SiGooglegemini } from "react-icons/si";
 
 function Footer() {
   return (
-    <div className='my-7 flex justify-center items-center'>
+    <div className='my-7 flex flex-col items-center border-t border-gray-300 pt-5'>
       <h2 className='text-center text-lg text-gray-500 inline-flex items-center'>
         Created By <a href='https://niketchawla.vercel.app/' target='_blank' rel="noopener noreferrer"> 
           <span className='text-[#7139f4] text-lg font-bold mx-1'>Niket Chawla</span>
         </a> | Powered By 
-        <a href='https://gemini.google.com/?hl=en-IN' target='_blank'><span className='text-[#7139f4] text-lg font-bold mx-1'> Google Gemini</span></a>
+        <a href='https://gemini.google.com/?hl=en-IN' target='_blank' rel="noopener noreferrer">
+          <span className='text-[#7139f4] text-lg font-bold mx-1'> Google Gemini</span>
+        </a>
         <SiGooglegemini className='text-[#7139f4] text-lg' />
       </h2>
     </div>
